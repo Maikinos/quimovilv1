bbd19fdbde4619930eba45be5469a6f5debaef14
Michael Vairo <mv.maikinos@gmail.com>
2018-11-29 11:14:36 -0500

    Add files via upload

---
+++ js/michael.js
@@ -0,0 +1,11 @@
+$("html").click(function() {
+    $('#accordion input.search-client').css({ "width": "0%", "padding": "0px" });
+});
+
+$( "span.fa-search" ).click(function(e) {
+    $(this).find('.search-client').css({ "width": "100%", "padding": "10px" });
+    e.stopPropagation();
+  });
+
+var compartirwhastsapp = $('.listado-productos').html();    
+$('#comparte-ws').attr('href','whatsapp://send?text=' + compartirwhastsapp);
